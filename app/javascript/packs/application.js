4e5e72dc5494e27bff9fdc637d32247b56b3ab1c
Baptiste Chebassier <baptiste.chebassier28@gmail.com>
2021-07-14 17:03:01 +0200

    some changes done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,9 +29,11 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { checkKey } from '../components/keyboard.js.erb'
+import { revealChart } from '../components/chart.js.erb'
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   checkKey()
+  revealChart()
 });
