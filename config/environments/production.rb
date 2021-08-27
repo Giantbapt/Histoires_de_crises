94ca14c3398b3d4485b3041ee6c109e46a961b95
Baptiste Chebassier <baptiste.chebassier28@gmail.com>
2021-08-27 13:14:06 +0200

    reverting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,14 +20,13 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = false
-  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
- # config.assets.compile = false
+  config.assets.compile = false
 
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
@@ -91,9 +90,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.serve_static_assets = false
-  config.serve_static_files = false
-  # config.assets.compile = true
+  config.serve_static_assets = true
+  config.assets.compile = true
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
